38e5375fee35bf0d4e94909998ac5291421a3806
Tran Ha Khuong Duy <khuongduy.tech@gmail.com>
2024-07-11 22:33:54 +0700

    A simple interactive program

---
+++ first-program/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace first_program;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //write to console
+        Console.WriteLine("Please enter your name:");
+        //read from console
+        string name = Console.ReadLine();
+        //write the string and additional argument to console
+        Console.WriteLine("Hello, {0}", name);
+        //keep program running till user clicks run
+        Console.ReadLine();
+    }
+}
