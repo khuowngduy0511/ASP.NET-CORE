1d2403d661458ff35a875eb48f814c0958ba98e3
Tran Ha Khuong Duy <khuongduy.tech@gmail.com>
2024-07-11 23:27:22 +0700

    Declaration and Initialization Of Data Types

---
+++ basics/variables/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace variables;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int intNumber = 30;
+        Console.WriteLine("Value of Integer is " +intNumber);
+        float floatNumber = 30.78f;
+        Console.WriteLine("Value of Float is " +floatNumber);
+        bool boolean = true;
+        Console.WriteLine("Value of Bool is " +boolean);
+        double doubleNumber = 45.1234;
+        Console.WriteLine("Value of Double is " +doubleNumber);
+        char charName = 'u';
+        Console.WriteLine("Value of Char is {0}", charName);
+    }
+}
