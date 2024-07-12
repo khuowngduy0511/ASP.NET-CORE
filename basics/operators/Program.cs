af3cf2c1acc3bc36248eb3b54abd7906422e50c3
Tran Ha Khuong Duy <khuongduy.tech@gmail.com>
2024-07-12 07:55:30 +0700

    Computing Area of Circle

---
+++ basics/operators/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace operators;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double pi = 3.14159;
+        Console.Write("Please enter a value for the radius: ");
+        double r = double.Parse(Console.ReadLine());
+        double area = pi*r*r;
+        Console.WriteLine("The area of circle with radius " + r + " is " + area);
+    }
+}
