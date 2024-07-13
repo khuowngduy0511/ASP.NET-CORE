21a81eca430f55d457604865885bf6c2f7c9700d
Tran Ha Khuong Duy <khuongduy.tech@gmail.com>
2024-07-13 23:42:47 +0700

    Score testing

---
+++ basics/conditions/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace conditions;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double score;
+        Console.Write("Enter your score: ");
+        score = double.Parse(Console.ReadLine()); 
+        Console.WriteLine("Your score: {0}",score);
+        if (score > 100)
+        { 
+            Console.WriteLine("Error: score is greater than 100!");
+        }
+        else if (score < 0) 
+        {
+            Console.WriteLine("Error: score is less than 0!");
+        }
+        else if (score >= 50) 
+        {
+            Console.WriteLine("Pass!");
+        }
+        else 
+        {
+            Console.WriteLine("Fail!");
+        }
+    }    
+}
+
