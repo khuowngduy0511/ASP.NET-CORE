92293c5a9eb20abbea97d89984095b7c67b3e01a
Tran Ha Khuong Duy <khuongduy.tech@gmail.com>
2024-07-14 23:29:08 +0700

    Pyramid Printing

---
+++ basics/loops/Ex_1/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Ex_1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Enter rows: ");
+        int rows = int.Parse(Console.ReadLine());
+         for(int i = 1; i <= rows; ++i){
+            for(int j = 1; j <= i; ++j){
+                Console.Write("a");
+                if(j != i ){
+                    Console.Write(" ");
+                }
+         }
+            if(i != rows){
+                Console.Write("\n");
+            }
+        }
+    }
+}
