3eb1c22b1a37949992415c9c259390df7928d31e
Tran Ha Khuong Duy <khuongduy.tech@gmail.com>
2024-07-14 23:52:37 +0700

    Fibonacci Sequence

---
+++ basics/loops/Ex_2/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Ex_2;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int first = 0, second = 1, fibonacci;
+        string ans = "";
+        Console.Write("Enter the number of the range : ");
+        int range = int.Parse(Console.ReadLine());
+
+        for(int i = 0; i <= range; i++){
+            
+            if(i <= 1){
+                fibonacci = i;
+                ans += fibonacci.ToString();
+            } else{
+                fibonacci = first + second;
+                first = second;
+                second = fibonacci;
+                ans += fibonacci.ToString();
+            }
+
+            if(i != (range - 1)){
+                ans += " ";
+            }
+        }
+
+        Console.WriteLine("Fibonacci Series upto {0} Terms: {1}",range, ans );
+    }
+}
