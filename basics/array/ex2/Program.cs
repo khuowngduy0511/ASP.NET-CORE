d16fa3a71bb88902bb8457f8686ecd3ad7b38369
Tran Ha Khuong Duy <khuongduy.tech@gmail.com>
2024-07-19 08:27:23 +0700

    Making Matrix Using Arrays

---
+++ basics/array/ex2/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace ex2;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Enter the size of the matrix: ");
+        int n = int.Parse(Console.ReadLine());
+        int[, ] matrix = new int[n, n];
+
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n; j++){
+                if(i == j){
+                    matrix[i, j] = 0;
+                }
+                else if(i > j){
+                    matrix[i, j] = -1;
+                }
+                else 
+                    matrix[i, j] = 1;
+            }
+        }
+
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n; j++){
+                Console.Write(matrix[i, j] +" ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
